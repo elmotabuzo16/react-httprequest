53911c94ac94fcac545be5474dea22ded7649974
Elmo <elmotabuzojr.ust@gmail.com>
2023-03-04 16:14:05 +0800

    refactoring..

--- src/App.js
+++ src/App.js
@@ -40,19 +40,26 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isError) {
+    content = <p>{isError}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !isError && (
-          <p>Found no movies</p>
-        )}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && isError && <p>{isError}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
